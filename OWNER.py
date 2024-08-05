b09305af188fd7f3fa1e1f7ed652c8e9d61590a9
CCAAQQA <zydb70509@gmail.com>
2024-08-05 13:30:33 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,11 +1,4 @@
-import re
-from os import getenv
-
-from dotenv import load_dotenv
-from pyrogram import filters
-
-load_dotenv()
-
+# متطلبات التنصيب #
 OWNER = ["H_8_o", "H_8_o"]
 OWNER_NAME = "𝐋𝐄𝐀𝐃𝐄𝐑 𝐒𝐀𝐃𝐃𝐀𝐌 𝐇𝐔𝐒𝐒𝐄𝐈𝐍⤸"
 BOT_TOKEN = "6711048210:AAGeTTCLCXyBmcgcgC264YPkmI5g5xBJSnQ"
@@ -13,6 +6,4 @@ DATABASE = "mongodb+srv://vv1vbvg:tahLdAddP2xj6R5b@cluster0.4xpikbs.mongodb.net"
 CHANNEL = "https://t.me/A1DIIU"
 GROUP = "https://t.me/SUPERXFAST"
 VIDEO = "https://t.me/H_8_ov/10"
-LOGS = "H_8_o"
-PHOTO = "https://telegra.ph/file/75737308889fade2c982a.jpg"
 LOGS = "H_8_o"
